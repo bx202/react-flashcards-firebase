4b0b6c498571ba1e43419a5a6c8d2442d01da4d7
rv328 <superpeko@yahoo.com>
2018-02-19 23:35:53 +0200

    Updated the App.js file

--- src/App.js
+++ src/App.js
@@ -3,10 +3,40 @@ import './App.css';
 import Card from './Card/Card'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      cards: [
+        {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
+        {id: 2, eng: "English_2", han: "Hanzi_2", pin: "Pinyin_2"}
+      ],
+      currentCard: {}
+    }
+  }
+
+  componentWillMount(){
+    const currentCards = this.state.cards
+
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)]
+    return (card)
+  }
+
   render() {
     return (
       <div className="App">
-        <Card />
+        <Card 
+          eng={this.state.currentCard.eng}
+          han={this.state.currentCard.han}
+          pin={this.state.currentCard.pin}
+        />
       </div>
     );
   }
